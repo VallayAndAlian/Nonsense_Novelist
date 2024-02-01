73cc601b87829e2aff6ff637234b42bc70a78cd9
TT TT <2322629088@qq.com>
2024-02-01 11:18:59 +0800

    新增草稿本的功能（初版）

--- Assets/Scripts/UIScripts/DraftUi.cs
+++ Assets/Scripts/UIScripts/DraftUi.cs
@@ -2,49 +2,373 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
+/// <summary>
+/// 挂在草稿本母节点上。
+/// </summary>
 public class DraftUi : MonoBehaviour
 {
+    //文本内容
+    List<Text> content;
 
     // 不换行的的空格符
     public static readonly string NO_BREAKING_SPACE = "\u00A0";//"\u3000";
 
-    public TMP_InputField inputField;
-    public TextMeshProUGUI text;
-    bool a = false;
+
+    bool select = false;
+
+    //墨水
+    bool inkRedOn = false;
+    int inkRedCount = 5;
+    Image inkRedObj;
+    bool inkBlackOn = false;
+    int inkBlackCount = 5;
+    Image inkBlackObj;
+    bool inkBlueOn = false;
+    int inkBlueCount = 5;
+    Image inkBlueObj;
+
+    //文本修改
+    int oriTextLen;
+    bool textHasChange;
+
+
     private void Start()
     {
-        inputField.text = "";
-      
-        text.gameObject.SetActive(true);
-        a = false;
-    }
-    private void Update()
-    {
-        if (inputField.isFocused&&(!a))
+        foreach (var _a in GetComponentsInChildren<TMP_InputField>())
         {
-            Debug.Log("inputField.isFocused");
-            Func1();
+            _a.text = "";
+            _a.transform.Find("showText").gameObject.SetActive(true);
+            _a.transform.Find("editText").gameObject.SetActive(false);
+            _a.caretColor -= new Color(0, 0, 0, 1);
         }
-        if (!inputField.isFocused && (a))
+
+        select = false;
+        //获取墨水本身
+        inkRedObj = this.transform.Find("red").GetComponent<Image>();
+        inkBlackObj = this.transform.Find("black").GetComponent<Image>();
+        inkBlueObj = this.transform.Find("blue").GetComponent<Image>();
+
+        //初始化设定
+        ChangeInkNum();
+        ClickInk(-1);
+    }
+
+    #region 开启草稿本的外部界面
+    public void openDraft()
+    {
+        CharacterManager.instance.pause = true;
+        this.gameObject.SetActive(true);
+        InitDraft();
+    }
+    public void closeDraft()
+    {
+        CharacterManager.instance.pause = false;
+        this.gameObject.SetActive(false);
+        //InitDraft();
+    }
+    void InitDraft()
+    {
+        ChangeInkNum();
+        ClickInk(-1);
+    }
+    #endregion
+
+
+    #region 墨水事件
+
+
+    /// <summary>
+    /// 外部点击墨水事件调用。0黑1红2蓝-1谁也没选
+    /// </summary>
+    /// <param name="_ink"></param>
+    public void ClickInk(int _ink)
+    {
+        switch (_ink)
         {
-            Debug.Log("inputField.isFocused");
-            Func2();
+            case 0://black
+                {//只是【进入状态】，是否做修改还不一定。因此此处不操作数字。
+                    if (inkBlackOn)
+                    { 
+                        DefualtState(); 
+                    }
+                    else if (IsInkEnough(_ink))
+                    {
+                        inkRedOn = false;
+                        inkBlackOn = true;
+                        inkBlueOn = false;
+                        inkBlackObj.color = Color.black;
+                        inkBlueObj.color = Color.blue;
+                        inkRedObj.color = Color.red;
+                        //
+                        OpenAllDrag();
+                        CloseAllDelete();
+                    }
+                }
+                break;
+            case 1://red
+                {
+                    if (inkRedOn)
+                    {
+                        DefualtState();
+                    }
+                    else if (IsInkEnough(_ink))
+                    {
+                        inkRedOn = true;
+                        inkBlackOn = false;
+                        inkBlueOn = false;
+                        inkBlackObj.color = Color.white;
+                        inkBlueObj.color = Color.blue;
+                        inkRedObj.color = Color.black;
+                        //
+                        CloseAllDrag();
+                        OpenAllDelete();
+                    }
+                }
+                break;
+            case 2://blue
+                {
+                    if (inkBlueOn)
+                    {
+                        DefualtState();
+                    }
+                    else if (IsInkEnough(_ink))
+                    {
+                        inkRedOn = false;
+                        inkBlackOn = false;
+                        inkBlueOn = true;
+                        inkBlackObj.color = Color.white;
+                        inkBlueObj.color = Color.black;
+                        inkRedObj.color = Color.red;
+                        //
+                        CloseAllDelete();
+                        CloseAllDrag();
+                    }
+                }
+                break;
+            case -1:
+                {
+                    DefualtState();
+                }
+                break;
+        }
+        ChangeInkNum();
+    }
+
+    void DefualtState()
+    {
+        inkRedOn = false;
+        inkBlackOn = false;
+        inkBlueOn = false;
+        inkBlackObj.color = Color.white;
+        inkBlueObj.color = Color.blue;
+        inkRedObj.color = Color.red;
+        //
+        CloseAllDrag();
+        CloseAllDelete();
+    }
+
+    /// <summary>
+    /// 外部表现变化
+    /// </summary>
+    public void ChangeInkNum()
+    {
+        inkRedObj.gameObject.GetComponentInChildren<Text>().color = Color.white;
+        inkBlackObj.gameObject.GetComponentInChildren<Text>().color = Color.white;
+        inkBlueObj.gameObject.GetComponentInChildren<Text>().color = Color.white;
+        inkRedObj.gameObject.GetComponentInChildren<Text>().text = inkRedCount.ToString();
+        inkBlackObj.gameObject.GetComponentInChildren<Text>().text = inkBlackCount.ToString();
+        inkBlueObj.gameObject.GetComponentInChildren<Text>().text = inkBlueCount.ToString();
+    }
+
+
+    /// <summary>
+    /// 打开所有的拖拽事件
+    /// </summary>
+    void OpenAllDrag()
+    {
+        var _all = this.GetComponentsInChildren<DragDraftText>();
+        foreach (var _a in _all)
+        {
+            _a.canDrag = true;
         }
     }
-    void Func1()
+
+
+
+    /// <summary>
+    /// 关闭所有的拖拽事件
+    /// </summary>
+    void CloseAllDrag()
     {
-        inputField.text = text.text;
-        text.gameObject.SetActive(false);
-        a = true;
+        var _all = this.GetComponentsInChildren<DragDraftText>();
+        foreach (var _a in _all)
+        {
+            _a.canDrag = false;
+        }
     }
-    void Func2()
+
+    void OpenAllDelete()
     {
+        var _all = this.GetComponentsInChildren<DragDraftText>();
+        foreach (var _a in _all)
+        {
+            _a.canDelete = true;
+        }
+    }
+    void CloseAllDelete()
+    {
+        var _all = this.GetComponentsInChildren<DragDraftText>();
+        foreach (var _a in _all)
+        {
+            _a.canDelete = false;
+        }
+    }
+    /// <summary>
+    /// 判断墨水数量是否足够.0黑1红2蓝
+    /// </summary>
+    /// <param name="_ink"></param>
+    public bool IsInkEnough(int _ink)
+    {
+        switch (_ink)
+        {
+            case 0:
+                if (inkBlackCount > 0) return true;
+                else { InkNotEnough(0);return false; }
+                
+            case 1:
+                if (inkRedCount > 0) return true;
+                else { InkNotEnough(1); return false; }
+            case 2:
+                if (inkBlueCount > 0) return true;
+                else { InkNotEnough(2); return false; }
+        }
+        return false;
+    }
+
+
+    /// <summary>
+    /// 0黑1红2蓝
+    /// </summary>
+    /// <param name="_ink"></param>
+    public void UseInkOnce(int _ink)
+    {
+        switch (_ink)
+        {
+            case 0:
+                { inkBlackCount -= 1; }
+                break;
+            case 1:
+                { inkRedCount -= 1; }
+                break;
+            case 2:
+                { inkBlueCount -= 1; }
+                break;
+        }
+        ChangeInkNum();
+    }
+
+
+    /// <summary>
+    /// 墨水不足时的外部提示.。0黑1红2蓝-1谁也没选
+    /// </summary>
+    /// <param name="_ink"></param>
+    public void InkNotEnough(int _ink)
+    {
+        switch (_ink)
+        {
+            case 0:
+                {
+                    inkBlackObj.gameObject.GetComponentInChildren<Text>().color = Color.red;                
+                }
+                break;
+            case 1:
+                {
+                    inkRedObj.gameObject.GetComponentInChildren<Text>().color = Color.red;
+                }
+                break;
+            case 2:
+                {
+                    inkBlueObj.gameObject.GetComponentInChildren<Text>().color = Color.red;
+                }
+                break;
+        }
+
+    }
+
+
+
+
+    #endregion
+
+
+    #region 文本编辑蓝墨水
+    /// <summary>
+    /// 实现文本编辑框
+    /// </summary>
+    public void OpenEditText(TMP_InputField _inputField)
+    {
+        if (select) return;
+        if (!inkBlueOn) return; 
+        if (!IsInkEnough(2))  return;
+        if (_inputField.GetComponent<DragDraftText>().hasDelete) return;
+
       
-        text.text = HandleTextContentFormat(inputField.text);
-        inputField.text = "";
-        text.gameObject.SetActive(true);
-        a = false;
+        _inputField.transform.Find("editText").gameObject.SetActive(true);
+        _inputField.caretColor+=new Color(0,0,0,1);
+        TextMeshProUGUI _text = _inputField.transform.Find("showText").GetComponent<TextMeshProUGUI>();
+        _inputField.text = _text.text;
+
+        oriTextLen = _text.text.Length;
+
+        if(!_inputField.GetComponent<DragDraftText>().hasChange)
+            textHasChange = false;
+
+        _text.gameObject.SetActive(false);
+        select = true;
     }
+
+    /// <summary>
+    /// 关掉文本编辑框
+    /// </summary>
+    public void CloseEditText(TMP_InputField _inputField)
+    {
+        if (!select) return;
+        if (!inkBlueOn) return;
+        if (!IsInkEnough(2)) return;
+        if (_inputField.GetComponent<DragDraftText>().hasDelete) return;
+
+
+        TextMeshProUGUI _text = _inputField.transform.Find("showText").GetComponent<TextMeshProUGUI>();
+        _text.text = HandleTextContentFormat(_inputField.text);
+      
+        _text.gameObject.SetActive(true);
+        select = false;
+
+        int count = _inputField.GetLineCount(); _inputField.text = "";
+        for (int x = 0; x < count-1; x++)
+        {
+            _inputField.text += "\n";
+        }
+        
+        if (textHasChange && (!_inputField.GetComponent<DragDraftText>().hasChange))
+        {
+            UseInkOnce(2);
+            _inputField.GetComponent<DragDraftText>().hasChange = true;
+        }
+
+        _inputField.transform.Find("editText").gameObject.SetActive(false);
+        _inputField.caretColor -= new Color(0, 0, 0, 1);
+    }
+
+
+
+    /// <summary>
+    /// 文本转换，处理unity的中英字符转行识别问题
+    /// </summary>
+    /// <param name="content"></param>
+    /// <returns></returns>
     public static string HandleTextContentFormat(string content)
     {
         if (content.Contains(" "))
@@ -54,10 +378,29 @@ public class DraftUi : MonoBehaviour
 
         return content;
     }
-    public void CheckContent()
+
+
+    /// <summary>
+    /// 外部：inputfield的on value change 中调用。
+    /// </summary>
+    public void CheckContent(TMP_InputField _inputField)
     {
-        inputField.text=HandleTextContentFormat(inputField.text);
+        if (!IsInkEnough(2)) return;
+        if (!select) return;
+        if (!inkBlueOn) return;
+        if (_inputField.GetComponent<DragDraftText>().hasDelete) return;
+
+        _inputField.text = HandleTextContentFormat(_inputField.text);
+        if (!textHasChange)
+        {
+            if (_inputField.text.Length != oriTextLen)
+                textHasChange = true;
+        }
+            
+          
     }
+
+    #endregion
 }
 
 
---
+++ Assets/Scripts/UIScripts/DragDraftText.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+/// <summary>
+/// 挂在每个句子组件身上。控制句子的拖拽，记录是否被修改、是否被删除等状态
+/// </summary>
+public class DragDraftText : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler,IPointerClickHandler
+{
+    //拖拽用
+    private Vector3 originPos;
+    private RectTransform rectTransform;
+    private Vector3 clickOffset;
+
+    //关联
+    private Transform parent;
+    private DraftUi draftUi;
+
+    //canDrag在进入黒墨水状态时开启；CanBegin在墨水次数大于0时开启
+    [HideInInspector]public bool canDrag = false;
+    private bool canBegin=false;
+
+    //外部红墨水和蓝墨水调用
+    [HideInInspector] public bool hasChange = false;
+    [HideInInspector] public bool hasDelete = false;
+    [HideInInspector] public bool canDelete = false;
+
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        parent = this.transform.parent;
+        draftUi = parent.parent.GetComponent<DraftUi>();
+        if (draftUi == null) Debug.LogWarning("draftUi找不到");
+    }
+
+    #region 拖拽事件
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+   
+        if (!canDrag) return;
+        if (!draftUi.IsInkEnough(0)) { canBegin = false; return; }
+        else  canBegin = true;
+
+        Vector3 pos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, eventData.enterEventCamera, out pos);
+        clickOffset = pos- rectTransform.position;
+        originPos = rectTransform.position;
+
+        parent.GetComponent<VerticalLayoutGroup>().enabled = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+        if (!canDrag) return;
+        if (!canBegin) return;
+
+        Vector3 pos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, eventData.enterEventCamera, out pos);
+        rectTransform.position = pos- clickOffset;
+       
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+       
+        if (!canDrag) return;
+        if (!canBegin) return;
+
+
+      //判断是否在某个ui之上
+        int changeIndex=-1;
+        int index = parent.childCount-1;
+
+
+        while (index>=0&& (changeIndex==-1))
+        {
+            if (parent.GetChild(index) != this.transform)
+            {
+               if(originPos.y >= parent.GetChild(index).GetComponent<RectTransform>().position.y)
+                {
+                    if ( rectTransform.position.y <= parent.GetChild(index).GetComponent<RectTransform>().position.y)
+                    {
+                        
+                        changeIndex = index;
+                        this.transform.SetSiblingIndex(index);
+                        draftUi.UseInkOnce(0);
+                    } 
+                }
+           
+            }
+            index--;
+        }
+
+
+         index =0;
+        while (index<parent.childCount && (changeIndex == -1))
+        {
+            if (parent.GetChild(index) != this.transform)
+            {
+                if (originPos.y <= parent.GetChild(index).GetComponent<RectTransform>().position.y)
+                {
+
+                    if (rectTransform.position.y >= parent.GetChild(index).GetComponent<RectTransform>().position.y)
+                    {
+
+                        changeIndex = index;
+                        this.transform.SetSiblingIndex(index);
+                        draftUi.UseInkOnce(0);
+                    }
+                }
+            }
+            index++;
+        }
+        //归零
+        rectTransform.position = originPos;
+        parent.GetComponent<VerticalLayoutGroup>().enabled = true;
+
+
+    }
+
+    #endregion
+
+
+    #region 点击事件
+    string _text;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!canDelete) return;
+
+        if (this.transform.Find("showText").GetComponent<TextMeshProUGUI>().text[0] == '<')
+        { hasDelete = true; } else { hasDelete = false; }
+
+
+        if ((!hasDelete)&&(draftUi.IsInkEnough(1)))
+        {
+           _text = this.transform.Find("showText").GetComponent<TextMeshProUGUI>().text;
+            this.transform.Find("showText").GetComponent<TextMeshProUGUI>().text = "<s>" + _text + "</s>";
+            draftUi.UseInkOnce(1);
+            hasDelete = true;
+            return;
+        }
+        if (hasDelete && (draftUi.IsInkEnough(1)))
+        {
+          
+            this.transform.Find("showText").GetComponent<TextMeshProUGUI>().text = _text;
+            draftUi.UseInkOnce(1);
+            hasDelete = false;
+            return;
+        }
+
+    }
+    #endregion
+
+}
