2f11fee70afa17d04cb260df1d60b74963fdeaf1
九尾 <2445493626@qq.com>
2024-05-27 19:31:35 +0800

    进度条素材更换
    lettersBUG隐藏

--- Assets/Scripts/UIScripts/Letter.cs
+++ Assets/Scripts/UIScripts/Letter.cs
@@ -12,7 +12,7 @@ public class Letter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FirstLetter();
+     // FirstLetter();
     }
 
     void FirstLetter()
