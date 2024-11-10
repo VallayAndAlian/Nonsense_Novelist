4029dab23b7f5fbe3821823e0e09a1109cf3e34a
TT TT <2322629088@qq.com>
2024-11-11 00:22:12 +0800

    角色血条和飘字，暂传

---
+++ Assets/Scripts/UI/BattleUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BattleUI : UIBase
+{
+
+  
+    public override void Start() { }
+
+    public override void Update(float deltaTime) { }
+
+    public override void LateUpdate(float deltaTime) { }
+}
